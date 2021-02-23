c4d5c4efaedde437e3770d141001ff2159e23398
pham thuong <ckpvthuongft@gmail.com>
2021-02-23 13:33:13 +0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+export SERVER_KEY=${SERVER_KEY:=""}
 lib/envsubst < config/mattermost-push-proxy-template.json > config/mattermost-push-proxy.json
 bin/mattermost-push-proxy --config=config/mattermost-push-proxy.json
